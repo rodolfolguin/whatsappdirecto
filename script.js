02f3e82686b336be18da9018d689601be79a468c
rodolfolguin <rodolfolguin2@gmail.com>
2022-04-06 20:07:04 -0400

    Actualizacion de front

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function iniciar(){
    var enlace = "https://wa.me/"
-   var numero = document.getElementById("numero").value;
+   var numero = document.getElementById("NumberInput").value;
    var unir = enlace + numero;
 
    alert("Desea iniciar conversacion con el numero: " + numero +" ?" )
    window.open(unir);
 }
+
+let box = new gsap.timeline();
+
+box.from(".container", { width: "0%", stagger: 0.4, duration: 1.5 });
+box.from(".form__title", { opacity: 0, y: -30 });
+box.from(".form__input-group input", { opacity: 0, stagger: 0.3 });
+
+box.from(".form__button ", { opacity: 0, y: 20 });
