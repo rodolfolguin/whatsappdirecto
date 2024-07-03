395c688e48341b18523e2241f05c34b50b42cac5
Rodolfolguin <73294694+rodolfolguin@users.noreply.github.com>
2024-07-03 15:42:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-function iniciar(){
-   var enlace = "https://wa.me/"
-   var numero = document.getElementById("NumberInput").value;
-   var unir = enlace + numero;
+function sendMessage() {
+   const prefixSelect = document.getElementById('prefix-select');
+   const phoneNumberInput = document.getElementById('phone-number');
+   const selectedPrefix = prefixSelect.value;
+   let phoneNumber = phoneNumberInput.value;
 
-   alert("Desea iniciar conversacion con el numero: " + numero +" ?" )
+   if (!phoneNumber.startsWith('+')) {
+      phoneNumber = selectedPrefix + phoneNumber;
+   }
+
+   alert('Número completo: ' + phoneNumber);
+   // Aquí puedes agregar el código para enviar el mensaje o procesar el número
+}
+
+function sendMessage() {
+   var enlace = "https://wa.me/"
+   var numero = document.getElementById("phone-number").value;
+   var prefix = document.getElementById("prefix-select").value;
+   var unir = enlace + prefix + numero;
+
+   alert("Desea iniciar conversacion con el numero: " + prefix + "" + numero + " ?")
    window.open(unir);
 }
 
 let box = new gsap.timeline();
 
-box.from(".container", { width: "0%", stagger: 0.4, duration: 1.5 });
-box.from(".form__title", { opacity: 0, y: -30 });
-box.from(".form__input-group input", { opacity: 0, stagger: 0.3 });
+box.from(".container", {
+   width: "0%",
+   stagger: 0.4,
+   duration: 1.5
+});
+box.from(".form__title", {
+   opacity: 0,
+   y: -30
+});
+box.from(".form__input-group input", {
+   opacity: 0,
+   stagger: 0.3
+});
 
-box.from(".form__button ", { opacity: 0, y: 20 });
+box.from(".form__button ", {
+   opacity: 0,
+   y: 20
+});
